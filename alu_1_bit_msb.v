7504a3de77b69b7f5516721f11b4c2426eaed0eb
Tolga Reis <tolgaareis@gmail.com>
2019-11-21 19:17:51 +0300

    Update alu_1_bit_msb.v

--- alu_1_bit_msb.v
+++ alu_1_bit_msb.v
@@ -1,29 +1,29 @@
 module alu_1bit_msb(op, a, b, c_in, lessi, r, c_out, set);
-	input [2:0] op;										//ADD: 010, SUB: 110, AND: 000, OR: 001, SLT: 111
+	input [2:0] op;					//ADD: 010, SUB: 110, AND: 000, OR: 001, SLT: 111
 	input a, b, c_in, lessi;
 	output r, c_out, set;
-	wire b_xor_op2, 										//to not use xor gate, it is created from and, or, not gates b'.op[2] + b.op[2]'
-		  b_not,												//negation of b input
-		  op2_not; 											//negation of op[2] input
+	wire b_xor_op2, 				//to not use xor gate, it is created from and, or, not gates b'.op[2] + b.op[2]'
+		  b_not,				//negation of b input
+		  op2_not; 				//negation of op[2] input
 	
-	not n1(b_not, b);										//b'
-	not n2(op2_not, op[2]);								//b[2]'
+	not n1(b_not, b);				//b'
+	not n2(op2_not, op[2]);				//b[2]'
 	
-	wire bn_and_op2, b_and_op2n;						//b'.op[2] + b.op[2]'
+	wire bn_and_op2, b_and_op2n;			//b'.op[2] + b.op[2]'
 	
-	and and1(bn_and_op2, b_not, op[2]); 			//b'.op[2]
-	and and2(b_and_op2n, b, op2_not);				//b.op[2]'
+	and and1(bn_and_op2, b_not, op[2]); 		//b'.op[2]
+	and and2(b_and_op2n, b, op2_not);		//b.op[2]'
 	or or1(b_xor_op2, bn_and_op2, b_and_op2n);	//b'.op[2] + b.op[2]'
 	
-	wire point_u,											//a.(b'.op[2] + b.op[2]')
-		  point_u_n,										//point_u' 	 
-		  point_v,											//a + (b'.op[2] + b.op[2]'), also the 1th input of mux
-		  point_w,								   		//point_u_n.point_v
-		  point_w_n,										//point_w'
-		  point_x,											//point_w.c_in
-		  point_x_n,										//point_x'
-		  point_y,											//point_w + c_in
-		  point_z;											//point_w_n.point_y
+	wire point_u,					//a.(b'.op[2] + b.op[2]')
+		  point_u_n,				//point_u' 	 
+		  point_v,				//a + (b'.op[2] + b.op[2]'), also the 1th input of mux
+		  point_w,				//point_u_n.point_v
+		  point_w_n,				//point_w'
+		  point_x,				//point_w.c_in
+		  point_x_n,				//point_x'
+		  point_y,				//point_w + c_in
+		  point_z;				//point_w_n.point_y
 		  
 	and and3(point_u, b_xor_op2, a);
 	not n3(point_u_n, point_u);
